e99dc365288e6486ced822a006cb7cd32d3c58ad
aRdiiaNaRdiiaN04 <youngcreative12@gmail.com>
2021-06-11 21:12:06 +0200

    Toggle+LocalStore

--- 10 - social-media-dashboard-with-theme-switcher-master/darkmode.js
+++ 10 - social-media-dashboard-with-theme-switcher-master/darkmode.js
@@ -1,11 +1,27 @@
+let darkMode = localStorage.getItem("darkMode");
 const darkModeToggle = document.querySelector("#dark-mode");
+const swicht = document.querySelector(".label");
+
 const enabledDarkMode = () => {
   document.body.classList.add("darkmode");
+  localStorage.setItem("darkMode", "enabled");
 };
+console.log();
 const disableDarkMode = () => {
   document.body.classList.remove("darkmode");
+  localStorage.setItem("darkMode", null);
 };
-
+if (darkMode === "enabled") {
+  enabledDarkMode();
+}
 darkModeToggle.addEventListener("click", () => {
-  document.body.classList.toggle("darkmode");
+  darkMode = localStorage.getItem("darkMode");
+  // document.body.classList.toggle("darkmode");
+  if (darkMode !== "enabled") {
+    enabledDarkMode();
+    swicht.classList.add("active");
+  } else {
+    disableDarkMode();
+    swicht.classList.remove("active");
+  }
 });
