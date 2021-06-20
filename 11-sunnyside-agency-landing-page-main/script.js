53a489e7e51327b2650e2f43dbb02f08e57309af
aRdiiaNaRdiiaN04 <youngcreative12@gmail.com>
2021-06-20 19:37:59 +0200

    Fix

--- 11-sunnyside-agency-landing-page-main/script.js
+++ 11-sunnyside-agency-landing-page-main/script.js
@@ -1,6 +1,5 @@
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
-console.log(menuBtn);
 menuBtn.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
